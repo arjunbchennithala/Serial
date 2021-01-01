c95b364133ec8095314810368ff1d36fb78080b3
Arjun B <68032142+arjunbchennithala@users.noreply.github.com>
2021-01-01 16:09:30 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,8 +1,9 @@
-#include "serial.h"
+#include "serial.h" //Including the library for seial communication
 int main(){
-    Serial s1("/dev/ttyACM0",115200);
-    s1.send("Hello");
-    cout<<"Recv : "<<s1.recv()<<endl;
-    s1.finish();
+    Serial s1("/dev/ttyACM0",115200); //Creating an serial object
+    s1.send("Hello");                 //Sending data to the serial port 
+    string data = s1.recv();          //Storing the data recieved from port
+    cout<<"Recv : "<<data<<endl;      //Printing
+    s1.finish();                      //Closing the port
     return 0;
 }
