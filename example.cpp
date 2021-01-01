305ec849e02a41fa61d15fc34b1d596b29998e5e
Arjun B <68032142+arjunbchennithala@users.noreply.github.com>
2021-01-01 16:11:42 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,6 +1,6 @@
 #include "serial.h" //Including the library for seial communication
 int main(){
-    Serial s1("/dev/ttyACM0",115200); //Creating an serial object
+    Serial s1("/dev/ttyACM0",115200); //Creating a serial object with its file location and baud rate.Leaving baud rate field will select the default rate=9600
     s1.send("Hello");                 //Sending data to the serial port 
     string data = s1.recv();          //Storing the data recieved from port
     cout<<"Recv : "<<data<<endl;      //Printing
