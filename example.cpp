ec60a5d6a20d6d6cf7c4c6a98558c896f2f07406
Arjun B <68032142+arjunbchennithala@users.noreply.github.com>
2021-01-01 16:06:15 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,7 +1,8 @@
 #include "serial.h"
 int main(){
-    Serial s("/dev/ttyACM0",115200);
-    s.send("Hello");
-    cout<<"Recv : "<<s.recv()<<endl;
+    Serial s1("/dev/ttyACM0",115200);
+    s1.send("Hello");
+    cout<<"Recv : "<<s1.recv()<<endl;
+    s1.finish();
     return 0;
 }
